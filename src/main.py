c1786096f7c4c9d7b2d5da95f134544443215fd8
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-10 01:08:24 +0530

    Changed to ant

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import gym
+import d4rl
 
 from agent import AWRAgent
 from models.actor import Actor
@@ -40,7 +41,7 @@ hyper_ps = {
 }
 
 # configuring the environment
-environment = gym.make('hopper-expert-v2')
+environment = gym.make("ant-expert-v0")
 # environment._max_episode_steps = 600
 
 # setting up the training components
