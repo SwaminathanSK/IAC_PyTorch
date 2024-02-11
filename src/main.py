248aef5dff8e6707bf929446e78456a57d36d010
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-12 01:29:28 +0530

    Changed hyperparams

--- src/main.py
+++ src/main.py
@@ -14,27 +14,27 @@ hyper_ps = {
     'replay_size': 50000,
     'max_epochs': 150,
     'sample_mod': 10,
-    'beta': 2.5,
-    'max_advantage_weight': 50.,
+    'beta': 0.25, # IAC
+    'max_advantage_weight': 200., # IAC
     'min_log_pi': -50.,
-    'discount_factor': .9,
-    'alpha': 0.95,
-    'c_hidden_size': 150,
-    'c_hidden_layers': 3,
-    'a_hidden_size': 200,
-    'a_hidden_layers': 5,
-    'c_learning_rate': 1e-5,
+    'discount_factor': .99, # IAC
+    'alpha': 0.95, 
+    'c_hidden_size': 256, # IAC
+    'c_hidden_layers': 2, # IAC
+    'a_hidden_size': 256, # IAC
+    'a_hidden_layers': 2, # IAC
+    'c_learning_rate': 3e-4, # IAC
     'c_momentum': .9,
-    'a_learning_rate': 5e-6,
-    'a_momentum': .9,
+    'a_learning_rate': 3e-4, # IAC
+    'a_momentum': .9, 
     'critic_threshold': 17.5,
     'critic_suffices_required': 1,
     'critic_steps_start': 200,
     'critic_steps_end': 200,
     'actor_steps_start': 1000,
-    'actor_steps_end': 1000,
-    'batch_size': 256,
-    'seed': 123456,
+    'actor_steps_end': 1000, 
+    'batch_size': 256, # IAC
+    'seed': 0, # IAC
     'replay_fill_threshold': 1.,
     'random_exploration': True,
     'test_iterations': 30,
@@ -52,7 +52,7 @@ environment = gym.make("halfcheetah-expert-v2")
 # setting up the training components
 agent = AWRAgent
 actor = Actor()
-critic = BootstrappedContinuousCritic({'target_update_rate' : 1})
+critic = BootstrappedContinuousCritic({'target_update_rate' : 0.005}) # IAC
 
 
 # training and testing
