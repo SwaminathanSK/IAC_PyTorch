357384bbe5ed75730f79f01aed57544fa05cc5ff
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-11 19:12:48 +0530

    debug

--- src/models/actor.py
+++ src/models/actor.py
@@ -49,16 +49,15 @@ class Actor(Model):
         logsd = self.fc_logsd(x)
         print("logsd", logsd.shape)
         # logsd = torch.clamp(self.logstd, -10, 2) 
-        sd = logsd.exp()
         print("mean", mean.shape)
         scale_tril = torch.diag(torch.exp(logsd))
-        batch_dim = 1
+        batch_dim = mean.shape[0]
         batch_scale_tril = scale_tril.repeat(batch_dim, 1, 1)
 
         normal = distributions.MultivariateNormal(loc=mean, scale_tril=batch_scale_tril)
         # normal = distributions.Normal(loc=mean, scale=sd)
         action = normal.sample()
-        # print(action)
+        print(action)
 
         return normal, action
 
