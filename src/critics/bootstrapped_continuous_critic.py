e466b323a0d8f2df466c248b354a04417d55d0e0
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-11 23:14:55 +0530

    print flags added

--- src/critics/bootstrapped_continuous_critic.py
+++ src/critics/bootstrapped_continuous_critic.py
@@ -126,6 +126,8 @@ class BootstrappedContinuousCritic(Critic):
         rewards = ptu.from_numpy(rewards)
         terminals = ptu.from_numpy(terminals)
 
+        print("Reached here")
+
         # q_pred = self.critic_network(torch.cat((obs, acts), dim=-1)).squeeze(1)
         q_pred = self.critic_network(obs)
         # target_value = self.compute_target_value(next_obs, rewards, terminals, actor)
