341ca2e5b506510927688c29f9e34d2f12e09164
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-11 09:02:18 +0530

    Changed ntraj

--- src/agent.py
+++ src/agent.py
@@ -62,7 +62,7 @@ class AWRAgent:
 
         # for i in range(20000):
         #     break
-        ntraj = 1000
+        ntraj = 10
         max_path_length = 100
         paths = utils.sample_n_trajectories(environment, beta_policy, ntraj, max_path_length)
         observations, actions, next_observations, terminals, concatenated_rewards, unconcatenated_rewards = AWRAgent.convert_listofrollouts(paths)
