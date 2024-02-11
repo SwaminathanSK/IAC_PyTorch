88cc4739146b0a6c85744f629c3f5038dcb37f87
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-11 10:20:23 +0530

    Debug

--- src/agent.py
+++ src/agent.py
@@ -124,7 +124,7 @@ class AWRAgent:
             states = torch.Tensor(states)
 
             # one-hot encoding
-            a = torch.LongTensor(actions).view(-1, 1)
+            a = torch.FloatTensor(actions).view(-1, 1)
 
             one_hot_action = torch.FloatTensor(2, 6).zero_()
             one_hot_action.scatter_(1, a, 1)
