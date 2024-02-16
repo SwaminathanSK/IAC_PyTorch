2eff7a6486536ffb8dd9385d68041057c9de953b
Swaminathan S K <swamisathya2004@gmail.com>
2024-02-16 07:03:18 +0530

    Changed params back to old

--- src/main.py
+++ src/main.py
@@ -10,31 +10,62 @@ from utilities.training import Training
 
 
 # setting the hyper-parameters
+# hyper_ps = {
+#     'replay_size': 50000,
+#     'max_epochs': 150,
+#     'sample_mod': 10,
+#     'beta': 0.25, # IAC
+#     'max_advantage_weight': 200., # IAC
+#     'min_log_pi': -50.,
+#     'discount_factor': .99, # IAC
+#     'alpha': 0.95, 
+#     'c_hidden_size': 256, # IAC
+#     'c_hidden_layers': 2, # IAC
+#     'a_hidden_size': 256, # IAC
+#     'a_hidden_layers': 2, # IAC
+#     'c_learning_rate': 3e-4, # IAC
+#     'c_momentum': .9,
+#     'a_learning_rate': 3e-4, # IAC
+#     'a_momentum': .9, 
+#     'critic_threshold': 17.5,
+#     'critic_suffices_required': 1,
+#     'critic_steps_start': 200,
+#     'critic_steps_end': 200,
+#     'actor_steps_start': 1000,
+#     'actor_steps_end': 1000, 
+#     'batch_size': 256, # IAC
+#     'seed': 0, # IAC
+#     'replay_fill_threshold': 1.,
+#     'random_exploration': True,
+#     'test_iterations': 30,
+#     'validation_epoch_mod': 3,
+# }
+
 hyper_ps = {
     'replay_size': 50000,
     'max_epochs': 150,
     'sample_mod': 10,
-    'beta': 0.25, # IAC
-    'max_advantage_weight': 200., # IAC
+    'beta': 2.5,
+    'max_advantage_weight': 50.,
     'min_log_pi': -50.,
-    'discount_factor': .99, # IAC
-    'alpha': 0.95, 
-    'c_hidden_size': 256, # IAC
-    'c_hidden_layers': 2, # IAC
-    'a_hidden_size': 256, # IAC
-    'a_hidden_layers': 2, # IAC
-    'c_learning_rate': 3e-4, # IAC
+    'discount_factor': .9,
+    'alpha': 0.95,
+    'c_hidden_size': 150,
+    'c_hidden_layers': 3,
+    'a_hidden_size': 200,
+    'a_hidden_layers': 5,
+    'c_learning_rate': 1e-4,
     'c_momentum': .9,
-    'a_learning_rate': 3e-4, # IAC
-    'a_momentum': .9, 
+    'a_learning_rate': 5e-5,
+    'a_momentum': .9,
     'critic_threshold': 17.5,
     'critic_suffices_required': 1,
     'critic_steps_start': 200,
     'critic_steps_end': 200,
     'actor_steps_start': 1000,
-    'actor_steps_end': 1000, 
-    'batch_size': 256, # IAC
-    'seed': 0, # IAC
+    'actor_steps_end': 1000,
+    'batch_size': 256,
+    'seed': 0,
     'replay_fill_threshold': 1.,
     'random_exploration': True,
     'test_iterations': 30,
